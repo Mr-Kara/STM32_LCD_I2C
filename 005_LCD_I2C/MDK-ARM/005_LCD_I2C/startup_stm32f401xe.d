d78828fe08b365fc674c527c52d8eb8864e2c968
Emre KARA <xemrekarax@gmail.com>
2022-01-18 21:40:00 +0300

    First commit of this project

---
+++ 005_LCD_I2C/MDK-ARM/005_LCD_I2C/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+005_lcd_i2c\startup_stm32f401xe.o: startup_stm32f401xe.s
---
+++ 005_LCD_I2C/MDK-ARM/RTE/_005_LCD_I2C/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '005_LCD_I2C' 
+ * Target:  '005_LCD_I2C' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
